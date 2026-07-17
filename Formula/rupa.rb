4852279ba10c4bbad0760662a4f4765f3a71f7c2
Gagas Sangga Pratama <repo@gagas.my.id>
2026-07-17 10:30:06 +0700

    Formula: bump to v1.0.3 with updated sha256

--- Formula/rupa.rb
+++ Formula/rupa.rb
@@ -2,7 +2,7 @@ class Rupa < Formula
   desc "Convert, extract audio from, and compress media"
   homepage "https://github.com/haiigas/homebrew-rupa"
   url "https://github.com/haiigas/homebrew-rupa/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "8d86ee311679b4804e15a3f5800b6dbbc05396c21fbb96d9450537c431949a11"
   license "MIT"
 
   depends_on "ffmpeg"
